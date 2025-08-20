e0280673ecc39f1b0e978be29b8ca03bf950dad9
unknown <pablogiroud@gmail.com>
2025-08-20 14:30:13 -0300

    old fashion

--- src/content_option.js
+++ src/content_option.js
@@ -126,8 +126,6 @@ const contactConfig = {
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: process.env.REACT_APP_YOUR_SERVICE_ID,
     YOUR_TEMPLATE_ID: process.env.REACT_APP_YOUR_TEMPLATE_ID,
-    PUBLIC_KEY: process.env.REACT_APP_PUBLIC_KEY,
-    CAPTCHA_KEY: process.env.REACT_APP_CAPTCHA_SITE_KEY,
 };
 
 const socialprofils = {
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -6,6 +6,8 @@ import { meta } from "../../content_option";
 import { Container, Row, Col, Alert } from "react-bootstrap";
 import { contactConfig } from "../../content_option";
 import ReCAPTCHA from "react-google-recaptcha";
+const { REACT_APP_PUBLIC_KEY } = process.env
+const { REACT_APP_CAPTCHA_SITE_KEY } = process.env;
 
 export const ContactUs = () => {
 
@@ -36,7 +38,7 @@ export const ContactUs = () => {
         contactConfig.YOUR_SERVICE_ID,
         contactConfig.YOUR_TEMPLATE_ID,
         templateParams,
-        contactConfig.PUBLIC_KEY
+        REACT_APP_PUBLIC_KEY
         //contactConfig.YOUR_USER_ID,
       )
       .then(
@@ -156,7 +158,7 @@ export const ContactUs = () => {
               <Row>
                 <Col lg="12" className="form-group">
                   <ReCAPTCHA
-                    sitekey={contactConfig.CAPTCHA_KEY}
+                    sitekey={REACT_APP_CAPTCHA_SITE_KEY}
                     onChange={(val) => setCaptcha(val)}
                   />
                   <button className="btn ac_btn my-3" type="submit" disabled={formData.loading || !captcha}>
